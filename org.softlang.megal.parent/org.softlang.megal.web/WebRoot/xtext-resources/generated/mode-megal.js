e83185a515d0b1df0f49b948540d9fccbc73ae89
Lukas Härtel <lukashaertel@googlemail.com>
2016-04-26 19:51:40 +0200

    More modifications, fallback to Xtype, nested structures, made some grammar tests

--- org.softlang.megal.parent/org.softlang.megal.web/WebRoot/xtext-resources/generated/mode-megal.js
+++ org.softlang.megal.parent/org.softlang.megal.web/WebRoot/xtext-resources/generated/mode-megal.js
@@ -1,21 +1,11 @@
 define(["ace/lib/oop", "ace/mode/text", "ace/mode/text_highlight_rules"], function(oop, mText, mTextHighlightRules) {
 	var HighlightRules = function() {
-		var keywords = "as|case|catch|default|do|else|extends|extension|false|finally|for|if|import|instanceof|jvm|new|null|return|static|super|switch|synchronized|throw|true|try|typeof|val|var|while";
+		var keywords = "extends|extension|import|jvm|static|super";
 		this.$rules = {
 			"start": [
-				{token: "comment", regex: "\\/\\/.*$"},
-				{token: "comment", regex: "\\/\\*", next : "comment"},
-				{token: "string", regex: '["](?:(?:\\\\.)|(?:[^"\\\\]))*?["]'},
-				{token: "string", regex: "['](?:(?:\\\\.)|(?:[^'\\\\]))*?[']"},
-				{token: "constant.numeric", regex: "[+-]?\\d+(?:(?:\\.\\d*)?(?:[eE][+-]?\\d+)?)?\\b"},
-				{token: "constant.numeric", regex: "0[xX][0-9a-fA-F]+\\b"},
 				{token: "lparen", regex: "[\\[({]"},
 				{token: "rparen", regex: "[\\])}]"},
 				{token: "keyword", regex: "\\b(?:" + keywords + ")\\b"}
-			],
-			"comment": [
-				{token: "comment", regex: ".*?\\*\\/", next : "start"},
-				{token: "comment", regex: ".+"}
 			]
 		};
 	};
